80c1c48ddbad78f363a1e438c4fc54c36668420e
Martin Schulze <37703201+martin-schulze-vireso@users.noreply.github.com>
2020-05-11 10:22:33 +0200

    Fix builds
    libtiff has some curious configurations that are missing prebuilts from the repo.

--- build.py
+++ build.py
@@ -5,6 +5,6 @@
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder(pure_c=False)
+    builder = build_template_default.get_builder(pure_c=False, build_policy='missing')
 
     builder.run()
