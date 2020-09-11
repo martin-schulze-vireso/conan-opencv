cc6b85830fbbce50b12781934a66545b60baeb7c
Martin Schulze <martin.schulze@vireso.de>
2020-09-11 17:14:37 +0200

    android: Only link cpufeatures in static builds
    OpenCV won't build the library in shared builds so we get a linker error

--- conanfile.py
+++ conanfile.py
@@ -599,10 +599,11 @@ class OpenCVConan(ConanFile):
         elif self.settings.os == 'Windows':
             self.cpp_info.system_libs.append('Vfw32')
         if self.settings.os == 'Android':
-            self.cpp_info.libs.extend(['log', 'cpufeatures'])
+            self.cpp_info.libs.append('log')
             self.cpp_info.includedirs.append(
                 os.path.join('sdk', 'native', 'jni', 'include'))
             if not self.options.shared:
+                self.cpp_info.libs.append('cpufeatures')
                 self.cpp_info.libdirs.append(
                     os.path.join('sdk', 'native', 'staticlibs', self._android_arch))
         else:
